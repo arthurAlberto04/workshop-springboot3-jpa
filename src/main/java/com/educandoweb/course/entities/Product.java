5bcc1f2465ff64b2704f4356e7980258394d7ea4
Arthur Alberto <arturalberto7@gmail.com>
2025-12-13 22:58:53 -0300

    Relacao Produto e orderitem

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -27,6 +28,9 @@ public class Product implements Serializable {
     )
     private Set<Category> categories =  new HashSet<>();
 
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
+
     public Product() {
     }
 
@@ -36,6 +40,16 @@ public class Product implements Serializable {
         this.description = description;
         this.price = price;
         this.name = name;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders(){
+        Set<Order> set = new HashSet<>();
+
+        for (OrderItem x : items){
+            set.add(x.getOrder());
+        }
+        return set;
     }
 
     public Long getId() {
