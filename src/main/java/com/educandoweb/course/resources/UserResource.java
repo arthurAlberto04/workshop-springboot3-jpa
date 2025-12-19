65276dff08b507efbd4a7797746f5890ecfb5cc0
Arthur Alberto <arturalberto7@gmail.com>
2025-12-19 00:04:19 -0300

    Delete User

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -36,4 +36,9 @@ public class UserResource {
                 .fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).body(obj);
     }
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
